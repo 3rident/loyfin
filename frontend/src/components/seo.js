2f7bb96d5f8e8403a6800419c84a2668afa345b8
Shubham Kukreti <shubhamkukreti204@gmail.com>
2021-09-24 02:55:32 +0530

    enhanced

--- frontend/src/components/seo.js
+++ frontend/src/components/seo.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Head from 'next/head';
 
 export default function SEO({
-  description = 'startup landing descriptions',
+  description = 'LoyFin descriptions',
   author = 'RedQ, Inc',
   meta,
-  title = 'startup landing title',
+  title = 'LoyFin title',
 }) {
   const metaData = [
     {
